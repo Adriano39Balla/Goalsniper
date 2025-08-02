778022ffabde634a4b383c92aac793f0d1bd1363
Adriano39Balla <adrianhotshot@gmail.com>
2025-08-02 11:31:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-import os
-
 API_KEY = os.getenv("API_KEY")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+DATABASE_URL = os.getenv("DATABASE_URL")
 MODEL_PATH = os.getenv("MODEL_PATH", "/app/model.pkl")
