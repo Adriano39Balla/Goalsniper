e94f2364189688826771b939adf78b4e4e050bdf
Adriano39Balla <adrianhotshot@gmail.com>
2025-08-02 11:15:14 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_KEY = os.getenv("API_KEY")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+MODEL_PATH = os.getenv("MODEL_PATH", "/app/model.pkl")
