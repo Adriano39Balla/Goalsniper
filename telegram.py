a0559ec1b3fa00609d42977710071a941835ddb2
Adriano39Balla <adrianhotshot@gmail.com>
2025-08-02 13:19:57 +0200

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -3,13 +3,13 @@ import os
 import requests
 import logging
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 logger = logging.getLogger(__name__)
 
 def send_telegram_message(message):
     try:
-        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
         payload = {"chat_id": CHAT_ID, "text": message}
         res = requests.post(url, data=payload, timeout=10)
         if res.status_code != 200:
