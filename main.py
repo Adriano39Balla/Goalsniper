52b11c0d1560db9cbeff84974e03acb725f5c89f
Adriano39Balla <adrianhotshot@gmail.com>
2025-08-08 18:21:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import logging
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+    )
+    app.run(host='0.0.0.0', port=5000, debug=app.config.get("DEBUG", False))
