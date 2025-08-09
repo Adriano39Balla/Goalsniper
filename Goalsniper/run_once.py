ea89528e67b22801d5961d6ba1c4de2a39194361
Adriano39Balla <adrianhotshot@gmail.com>
2025-08-09 15:58:37 +0200

    Delete Goalsniper directory

--- Goalsniper/run_once.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from scanner import run_scan_and_send
-
-def main():
-    result = asyncio.run(run_scan_and_send())
-    print("Run finished", result, flush=True)
-
-if __name__ == "__main__":
-    main()
