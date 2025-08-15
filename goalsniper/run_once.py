ec4846eee2527a1b05c5c50ec33109f19cdb3818
Adriano39Balla <adrianhotshot@gmail.com>
2025-08-15 10:46:52 +0200

    Update run_once.py

--- goalsniper/run_once.py
+++ goalsniper/run_once.py
@@ -1,9 +1,6 @@
+# goalsniper/run_once.py
 import asyncio
 from .scanner import run_scan_and_send
 
-def main():
-    result = asyncio.run(run_scan_and_send())
-    print("Run finished", result, flush=True)
-
 if __name__ == "__main__":
-    main()
+    asyncio.run(run_scan_and_send())
