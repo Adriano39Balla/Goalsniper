47eabe369336bd4e55c1387fda6d4d8fb1452224
Adriano39Balla <adrianhotshot@gmail.com>
2025-09-19 12:51:54 +0200

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,9 +1,10 @@
-# file: gunicorn_conf.py
-bind = "0.0.0.0:{}".format(__import__("os").environ.get("PORT", "8080"))
-workers = 1                  # keep 1 so scheduler runs once
+# gunicorn_conf.py
+import os
+bind = f"0.0.0.0:{os.environ.get('PORT','8080')}"
+workers = 1
 worker_class = "gthread"
-threads = 8
-timeout = 180
+threads = 4          # lower to reduce RAM
+timeout = 120
 graceful_timeout = 30
 loglevel = "info"
 accesslog = "-"
