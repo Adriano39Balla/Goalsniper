5a3a43c02db13f3a773cc1791a239479a9209b08
Adriano39Balla <adrianhotshot@gmail.com>
2025-12-14 10:32:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1242,5 +1242,5 @@ if __name__ == '__main__':
     initialize_system()
     
     # Run Flask app
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
